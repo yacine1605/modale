41eb880adccc5500da553efcecca216756c9c705
yacine1605 <77847874+yacine1605@users.noreply.github.com>
2021-05-18 21:50:44 +0100

    Update auth.js

--- midddleware/auth.js
+++ midddleware/auth.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 
 const authUser = (req, res, next) => {
 	try {
-		//	const token = req.headers.authorization ? req.headers.authorization.split('')[1] : '';
-		const token = req.headers['x-access-token'];
+			const token = req.headers.authorization ? req.headers.authorization.split('')[1] : '';
+		//const token = req.headers['x-access-token'];
 
 		if (!token) {
 			const error = new Error('access denied');
