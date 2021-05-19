5da6c8cab5f6dd3b34cc236ac56990bf76f71154
yacine1605 <77847874+yacine1605@users.noreply.github.com>
2021-05-19 16:29:50 +0200

    Update auth.js

--- midddleware/auth.js
+++ midddleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const authUser = (req, res, next) => {
 	try {
-			const token = req.headers.authorization ? req.headers.authorization.split('')[1] : '';
+			const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : '';
 		//const token = req.headers['x-access-token'];
 
 		if (!token) {
